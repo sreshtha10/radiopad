3dfb2e852c9772cfe4577fc6054e4247de5be3bf
Sreshtha <sreshtha.mehrotra@gmail.com>
2022-05-25 03:33:52 +0530

    implemented basic ui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:radiopad/utils/all_util.dart';
 import 'package:velocity_x/velocity_x.dart';
+
 import '../model/radio.dart';
 
 class HomePage extends StatefulWidget {
@@ -22,9 +25,9 @@ class _HomePageState extends State<HomePage> {
           VxAnimatedBox()
               .size(context.screenWidth, context.screenHeight)
               .withGradient(LinearGradient(colors: [
-            AllColors.primaryColor,
-            AllColors.primaryColor2,
-          ], begin: Alignment.topLeft, end: Alignment.bottomRight))
+                AllColors.primaryColor,
+                AllColors.primaryColor2,
+              ], begin: Alignment.topLeft, end: Alignment.bottomRight))
               .make(),
           AppBar(
             title: "RADIOPAD".text.xl4.bold.white.make().shimmer(
@@ -33,25 +36,69 @@ class _HomePageState extends State<HomePage> {
             elevation: 0.0,
             centerTitle: true,
           ).h(100.0).p16(),
+          SizedBox(
+            height: 100,
+          ),
           VxSwiper.builder(
-              itemCount: radios!.length,
+              itemCount: radios.length,
               aspectRatio: 1.0,
               itemBuilder: (context, index) {
-                final rad = radios![index];
-                return VxBox(child: ZStack([]))
+                final rad = radios[index];
+                return VxBox(
+                        child: ZStack([
+                 Positioned(
+                   top: 0.0,
+                   right: 0.0,
+                   child: VxBox(
+                     child: rad.category.text.uppercase.white.make().px16(),
+                   ).height(40).black.alignCenter.withRounded(value: 10).make(),
+                 ) ,
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: VStack(
+                      [
+                        rad.name.text.xl3.white.bold.make(),
+                        5.heightBox,
+                        rad.tagline.text.sm.white.semiBold.make(),
+                      ],
+                      crossAlignment: CrossAxisAlignment.center,
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: [
+                      const Icon(
+                        CupertinoIcons.play_circle,
+                        color: Colors.white,
+                      ),
+                      10.heightBox,
+                      "Double Tap to Play".text.gray300.make(),
+                    ].vStack(),
+                  )
+                ],
+                          clip: Clip.antiAlias,
+                        ))
+                    .clip(Clip.antiAlias)
                     .bgImage(DecorationImage(
-                    image: NetworkImage(rad.image),
-                    fit: BoxFit.cover,
-                    colorFilter: ColorFilter.mode(
-                        Colors.black.withOpacity(0.5), BlendMode.darken)))
-                    .withRounded(value: 40.0)
+                        image: NetworkImage(rad.image),
+                        fit: BoxFit.cover,
+                        colorFilter: ColorFilter.mode(
+                            Colors.black.withOpacity(0.5), BlendMode.darken)))
+                    .withRounded(value: 60.0)
+                    .border(
+                        color: Colors.black,
+                        width: 1.0,
+                        style: BorderStyle.solid)
                     .make()
+                    .onInkDoubleTap(() {
+
+                    })
                     .p16()
                     .centered();
               })
         ],
+        fit: StackFit.expand,
       ),
     );
-
   }
 }
