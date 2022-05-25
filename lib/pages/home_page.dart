b134a9f0c26d076860dfa6967c14b3ff22ad63be
Sreshtha <sreshtha.mehrotra@gmail.com>
2022-05-25 06:58:27 +0530

    integrated audio player

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:radiopad/utils/all_util.dart';
 import 'package:velocity_x/velocity_x.dart';
@@ -15,6 +15,32 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<MyRadio> radios = myRadios;
+  MyRadio _selectedRadio = myRadios[0];
+  Color? _selectedColor = null;
+  bool _isPlaying = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _audioPlayer.onPlayerStateChanged.listen((event) {
+      if (event == PlayerState.PLAYING) {
+        _isPlaying = true;
+      } else {
+        _isPlaying = false;
+      }
+      setState(() {
+      });
+    });
+  }
+
+  final AudioPlayer _audioPlayer = AudioPlayer();
+
+  play(String url)  {
+    _audioPlayer.play(url);
+    _selectedRadio = radios.firstWhere((element) => element.url == url);
+  }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,48 +62,51 @@ class _HomePageState extends State<HomePage> {
             elevation: 0.0,
             centerTitle: true,
           ).h(100.0).p16(),
-          SizedBox(
-            height: 100,
-          ),
           VxSwiper.builder(
               itemCount: radios.length,
               aspectRatio: 1.0,
               itemBuilder: (context, index) {
                 final rad = radios[index];
                 return VxBox(
-                        child: ZStack([
-                 Positioned(
-                   top: 0.0,
-                   right: 0.0,
-                   child: VxBox(
-                     child: rad.category.text.uppercase.white.make().px16(),
-                   ).height(40).black.alignCenter.withRounded(value: 10).make(),
-                 ) ,
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: VStack(
-                      [
-                        rad.name.text.xl3.white.bold.make(),
-                        5.heightBox,
-                        rad.tagline.text.sm.white.semiBold.make(),
-                      ],
-                      crossAlignment: CrossAxisAlignment.center,
+                        child: ZStack(
+                  [
+                    Positioned(
+                      top: 0.0,
+                      right: 0.0,
+                      child: VxBox(
+                        child: rad.category.text.uppercase.white.make().px16(),
+                      )
+                          .height(40)
+                          .black
+                          .alignCenter
+                          .withRounded(value: 10)
+                          .make(),
                     ),
-                  ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: [
-                      const Icon(
-                        CupertinoIcons.play_circle,
-                        color: Colors.white,
+                    Align(
+                      alignment: Alignment.bottomCenter,
+                      child: VStack(
+                        [
+                          rad.name.text.xl3.white.bold.make(),
+                          5.heightBox,
+                          rad.tagline.text.sm.white.semiBold.make(),
+                        ],
+                        crossAlignment: CrossAxisAlignment.center,
                       ),
-                      10.heightBox,
-                      "Double Tap to Play".text.gray300.make(),
-                    ].vStack(),
-                  )
-                ],
-                          clip: Clip.antiAlias,
-                        ))
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: [
+                        const Icon(
+                          CupertinoIcons.play_circle,
+                          color: Colors.white,
+                        ),
+                        10.heightBox,
+                        "Double Tap to Play".text.gray300.make(),
+                      ].vStack(),
+                    )
+                  ],
+                  clip: Clip.antiAlias,
+                ))
                     .clip(Clip.antiAlias)
                     .bgImage(DecorationImage(
                         image: NetworkImage(rad.image),
@@ -91,11 +120,29 @@ class _HomePageState extends State<HomePage> {
                         style: BorderStyle.solid)
                     .make()
                     .onInkDoubleTap(() {
-
-                    })
-                    .p16()
-                    .centered();
+                  play(rad.url);
+                }).p16();
+              }).centered(),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: [
+              if (_isPlaying)
+                "Playing now =  ${_selectedRadio.name} FM".text.makeCentered(),
+              Icon(
+                _isPlaying
+                    ? CupertinoIcons.stop_circle
+                    : CupertinoIcons.play_circle,
+                color: Colors.white,
+                size: 50,
+              ).onInkTap(() {
+                if (_isPlaying) {
+                  _audioPlayer.stop();
+                } else {
+                  play(_selectedRadio.url);
+                }
               })
+            ].vStack(),
+          ).p64()
         ],
         fit: StackFit.expand,
       ),
